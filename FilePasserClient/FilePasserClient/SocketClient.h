da7c8c6f4d2311611439bb09c284ec28e8a81e9c
A6ly <a6ly26@gmail.com>
2020-01-29 17:47:34 +0900

    2020.01.29

---
+++ FilePasserClient/FilePasserClient/SocketClient.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+// SocketClient 명령 대상
+
+#define WM_CLIENT_RECV WM_USER+1
+
+class SocketClient : public CSocket
+{
+public:
+	SocketClient();
+	virtual ~SocketClient();
+	void SetWnd(HWND hWnd);
+	HWND m_hWnd;
+	virtual void OnReceive(int nErrorCode);
+	virtual void OnClose(int nErrorCode);
+};
+
+
